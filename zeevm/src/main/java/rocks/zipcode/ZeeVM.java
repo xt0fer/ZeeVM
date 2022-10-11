9f45b9e25e5b55bd6a09d4c4aa9f3d4528386d90
KrYounger <35814586+xt0fer@users.noreply.github.com>
2022-10-11 16:46:08 -0400

    initial

---
+++ zeevm/src/main/java/rocks/zipcode/ZeeVM.java
@@ -0,0 +1,13 @@
+package rocks.zipcode;
+
+/**
+ * Hello world!
+ *
+ */
+public class ZeeVM 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
